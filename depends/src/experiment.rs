115de10dae5cf3e1da5eefc934a7211de0f4fbb1
Otto Castle <otto.castle1@gmail.com>
2024-06-23 18:43:12 +0100

    okok gen

--- depends/src/experiment.rs
+++ depends/src/experiment.rs
@@ -1,47 +1,56 @@
 use crate::error::ResolveResult;
 use crate::{DepRef, Dependency, HashValue, IsDirty, Resolve, SingleRef, Visitor};
 
-pub struct ThangDep<A, B> {
+pub struct ThangDep<A, B, C> {
     pub a: Dependency<A>,
     pub b: Dependency<B>,
+    pub c: Dependency<C>,
 }
-impl<A, B> ThangDep<A, B>
-where
-    A: Resolve,
-    B: Resolve,
-    for<'a> <A as Resolve>::Output<'a>: HashValue,
-    for<'a> <B as Resolve>::Output<'a>: HashValue,
-{
-    pub fn new(a: A, b: B) -> Self {
-        Self {
-            a: Dependency::new(a),
-            b: Dependency::new(b),
-        }
-    }
-}
-pub struct ThangRef<'a> {
-    pub this: DepRef<'a, i32>,
-    pub that: DepRef<'a, String>,
-}
-impl IsDirty for ThangRef<'_> {
-    fn is_dirty(&self) -> bool {
-        self.this.is_dirty() || self.that.is_dirty()
-    }
-}
-impl<A, B> Resolve for ThangDep<A, B>
+impl<A, B, C> ThangDep<A, B, C>
 where
     for<'a> A: Resolve<Output<'a> = i32> + 'a,
     for<'a> B: Resolve<Output<'a> = String> + 'a,
+    for<'a> C: Resolve + 'a,
     for<'a> <A as Resolve>::Output<'a>: HashValue,
     for<'a> <B as Resolve>::Output<'a>: HashValue,
+    for<'a> <C as Resolve>::Output<'a>: HashValue,
 {
-    type Output<'a> = ThangRef<'a>
+    pub fn new(a: A, b: B, c: C) -> Self {
+        Self {
+            a: Dependency::new(a),
+            b: Dependency::new(b),
+            c: Dependency::new(c),
+        }
+    }
+}
+pub struct ThangRef<'a, T> {
+    pub this: DepRef<'a, i32>,
+    pub that: DepRef<'a, String>,
+    pub him: DepRef<'a, T>,
+}
+impl<T> IsDirty for ThangRef<'_, T> {
+    fn is_dirty(&self) -> bool {
+        self.this.is_dirty() || self.that.is_dirty() || self.him.is_dirty()
+    }
+}
+impl<A, B, C> Resolve for ThangDep<A, B, C>
+where
+    for<'a> A: Resolve<Output<'a> = i32> + 'a,
+    for<'a> B: Resolve<Output<'a> = String> + 'a,
+    for<'a> C: Resolve + 'a,
+    for<'a> <A as Resolve>::Output<'a>: HashValue,
+    for<'a> <B as Resolve>::Output<'a>: HashValue,
+    for<'a> <C as Resolve>::Output<'a>: HashValue,
+// TODO: constraints for C::Output
+{
+    type Output<'a> = ThangRef<'a, <C as Resolve>::Output<'a>>
         where Self: 'a;
 
     fn resolve(&self, visitor: &mut impl Visitor) -> ResolveResult<Self::Output<'_>> {
         Ok(ThangRef {
             this: self.a.resolve(visitor)?,
             that: self.b.resolve(visitor)?,
+            him: self.c.resolve(visitor)?,
         })
     }
 }
